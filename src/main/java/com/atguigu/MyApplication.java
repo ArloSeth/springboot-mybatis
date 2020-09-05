935f5b4f97085cfd97e72c54971c7dc52b607a43
lizheqi <lz2230867392@gmail.com>
2020-09-05 11:56:41 +0800

    springboot-mybatis提交111

--- src/main/java/com/atguigu/MyApplication.java
+++ src/main/java/com/atguigu/MyApplication.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MyApplication {
 
     public static void main(String[] args) {
+        System.out.println(111);
         SpringApplication.run(MyApplication.class,args);
     }
 }
